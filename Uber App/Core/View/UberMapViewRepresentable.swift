eea58f38e3cdf9653cdf86e152a2f00c49c6f69c
Deepam Dhakal <dhakaldiipam@gmail.com>
2023-10-17 15:51:53 +0545

    Initial Commit

--- Uber App/Core/View/UberMapViewRepresentable.swift
+++ Uber App/Core/View/UberMapViewRepresentable.swift
@@ -9,32 +9,49 @@ import SwiftUI
 import MapKit
 
 struct UberMapViewRepresentable: UIViewRepresentable {
-    let mapkit = MKMapView()
-    let locationManager = LocationManager()
+
+    let mapView = MKMapView()
+    let locationManager = LocationManager()  
 
 
     func makeUIView(context: Context) -> some UIView {
 //whenever we call uiviewrepresentable this property helps to initialize and it always return the initialized value
-        mapkit.isRotateEnabled = false
-        mapkit.showsUserLocation = true
-        mapkit.userTrackingMode = .follow
 
-        return mapkit
+        mapView.delegate = context.coordinator
+        mapView.isRotateEnabled = false
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+
+        return mapView
     }
 
     func updateUIView(_ uiView: UIViewType, context: Context) {
 //        hello
     }
 
+    func makeCoordinator() -> MapCoordinator {
+        return MapCoordinator(parent: self)
+    }
 }
 
 extension UberMapViewRepresentable {
 
     class MapCoordinator: NSObject, MKMapViewDelegate {
         let parent: UberMapViewRepresentable
-        init ( parent: UberMapViewRepresentable) {
+
+        init (parent: UberMapViewRepresentable) {
             self.parent = parent
             super.init()
+        }
+
+        func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            let region = MKCoordinateRegion(
+                center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude,
+                                               longitude: userLocation.coordinate.longitude)
+                ,
+                span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+
+            parent.mapView.setRegion(region, animated: true)
         }
     }
 }
--- Uber App/Managers/LocationManager.swift
+++ Uber App/Managers/LocationManager.swift
@@ -13,8 +13,10 @@ class LocationManager: NSObject, ObservableObject {
     private let locationManager = CLLocationManager()
 
     override init() {
+        super.init()
 //        we have to initialze it inorder to active the location of the user
 
+        locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
 //        give the best accuracy of the user
         locationManager.requestWhenInUseAuthorization()
@@ -22,5 +24,12 @@ class LocationManager: NSObject, ObservableObject {
         locationManager.startUpdatingLocation()
 //        tracks the lcoaiton of the user in real time
         
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard !locations.isEmpty else { return }
+        locationManager.stopUpdatingLocation()
     }
 }
